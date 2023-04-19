8aeb14f933ed7209fcfa71b79fdea0b10d6636f3
Jason Alberto <alberto.jason@gmail.com>
2023-04-19 17:35:14 -0400

    Toggle user tokens and name and enable play button on game selection

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 // VARIABLES & DATA MODEL
 
-var gameOptions = ['rock', 'paper', 'scissor', 'lizard', 'spock'];
+var gameOptions = ['rock', 'paper', 'scissor', 'spock', 'lizard'];
 var currentGame = [];
-var players = [
-  {
-    name: 'Human',
-    score: 0
-  }, {
-    name: 'Computer',
-    score: 0
-  }];
+var players = {
+  user: { token: '🙂', name: 'Human', score: 0 }, 
+  comp: { name: 'Computer', score: 0 }
+};
 
   var gameVersion = document.querySelector('.game-selection');
+  var gameButton = document.querySelector('button');
+  var playerToken = document.querySelector('.player-token');
+  var playerBanner = {
+    token: document.querySelector('#token'),
+    name: document.querySelector('.user-name'),
+    score: document.querySelector('.player-wins'),
+  };
+  var computerScore = document.querySelector('computer-wins');
+  var standardGame = document.querySelector('.standard-game');
+  var variationGame = document.querySelector('.alien-game')
+                      // var gameArea = document.querySelector('main');
+  gameButton.disabled = true;
 
 // EVENT LISTENERS 
 
+playerToken.addEventListener('click', function(event) {
+  adjustPlayer(event, players);
+});
+
 gameVersion.addEventListener('click', function(event) {
-  // console.log( )
+  enableGameBackgroundToggle(event);
+  enableButton();
   getGameVersion(event);
 });
+
+gameButton.addEventListener('click', function(event) {
+
+});
+
+// gameArea.addEventListener('click', function(event) {
+  // console.log(event)
+  // getGameVersion(event);
+// });
 
 // EVENT HANDLERS AND OTHER FUNCTIONS
 
@@ -35,10 +57,19 @@ function getPlayerWepon(gameWepons, event) {
   }
 }
 
+function adjustPlayer(event, players) {
+  if (event.target.classList.contains('token')) {
+    players.user.token = event.target.innerText;
+    players.user.name = event.target.id;
+    playerBanner.token.innerText = `${players.user.token}`;
+    playerBanner.name.innerText = `${players.user.name}`;
+  }
+}
+
 function getGameLogic(player, comp) {
   var logicWeapons = currentGame.slice(0, currentGame.length);
   var cutLogicWeapons = logicWeapons.slice(0, player);
-  
+
   for (var i = 0; i < cutLogicWeapons.length; i++) {
     logicWeapons.push(cutLogicWeapons[i])
   }
@@ -61,4 +92,23 @@ function getGameVersion(event) {
     currentGame = gameOptions.slice(0, 5)
   }
   return currentGame;
+}
+
+function enableButton() {
+  gameButton.disabled = false;
+  gameButton.style.backgroundColor = '#4D194D'
+}
+
+function enableGameBackgroundToggle(event) {
+  if (event.target.classList.contains('standard-game') || event.target.parentNode.classList.contains('standard-game')) {
+    standardGame.style.backgroundColor = '#4D194D';
+    variationGame.style.backgroundColor = '#4D194D65';
+  } else if (event.target.classList.contains('alien-game') || event.target.parentNode.classList.contains('alien-game')) {
+    standardGame.style.backgroundColor = '#4D194D65';
+    variationGame.style.backgroundColor = '#4D194D';
+  }
+}
+
+function toggleGameView() {
+
 }
